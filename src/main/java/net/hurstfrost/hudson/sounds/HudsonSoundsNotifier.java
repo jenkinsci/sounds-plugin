092b641c2a75abdefb9e903ffb70d4191fb05898
Ed Hurst-Frost <edward@hurst-frost.net>
2016-06-25 16:44:06 +0100

    Merge remote-tracking branch 'remotes/origin/master' into upgrade
    # Conflicts:
    #	src/main/java/net/hurstfrost/hudson/sounds/HudsonSoundsNotifier.java

--- src/main/java/net/hurstfrost/hudson/sounds/HudsonSoundsNotifier.java
+++ src/main/java/net/hurstfrost/hudson/sounds/HudsonSoundsNotifier.java
@@ -707,20 +707,4 @@ public class HudsonSoundsNotifier extends Notifier {
 		
 		return foundEvent;
 	}
-<<<<<<< HEAD
-=======
-
-	protected static String toUri(String path) {
-		if (StringUtils.isEmpty(path)) {
-			return "";
-		}
-		
-		if (path.startsWith("http://") || path.startsWith("file:/")) {
-			return path;
-		}
-		
-		// Try to make sense of this as a filing system path
-		return new File(path).toURI().toString();
-	}
->>>>>>> remotes/origin/master
 }
